d97e4699c9d5eb2899fedc7b12629bba85fd116f
Anonymous5667 <115745181+Anonymous5667@users.noreply.github.com>
2022-10-14 03:23:58 +0530

    Add files via upload

---
+++ Samridhi_Yadav/marketing-agency-landing-page-main/marketing-agency/src/carouselData.js
@@ -0,0 +1,32 @@
+const carouselData = [
+  {
+    message:
+      "Not weekly or monthly like other sites out there. This ensures that we offer prospective homebuyers and investors with the freshest- hottest deals on the Internet.",
+    name: "Rowhan Smith",
+    position: "CEO, Foreclosure",
+    img: "./assets/tstmnl_ig_img2.png",
+  },
+  {
+    message:
+      "Deeds are to be worshipped, not statues. And maybe even deeds do not need to be worshipped. ",
+    name: "Elon Kumar Musk",
+    position: "CEO, Tesla",
+    img: "./assets/tstmnl_ig_img2.png",
+  },
+  {
+    message:
+      "We all need people who will give us feedback. That’s how we improve.",
+    name: "Bill gates yadav",
+    position: "co-founder, Microsoft",
+    img: "./assets/tstmnl_ig_img2.png",
+  },
+  {
+    message:
+      "People regardless of age are able to do a good job if they're inspired by the leader they work with.",
+    name: "Ritesh Agarwal",
+    position: "CEO, OYO Rooms",
+    img: "./assets/tstmnl_ig_img2.png",
+  },
+];
+
+export default carouselData;
